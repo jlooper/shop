b0dec36797018af20690da546ff2b5ba850888f0
Jen Looper <jen.looper@gmail.com>
2024-11-17 20:44:54 -0500

    adding pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+    integrations: [tailwind(), sanity({
+        projectId: "a3jw7085",
+        dataset: "production",
+        useCdn: false, // for static builds
+      }),
+    ],
 });
